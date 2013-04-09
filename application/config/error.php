ab45a03a93f8e4bfa54037da3c77439080840d32
Mark Myers <marcusmyers@gmail.com>
2013-04-09 15:34:26 -0400

    fix for production use...

--- application/config/error.php
+++ application/config/error.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'ignore' => array(),
+	'ignore' => array(E_WARNING, E_NOTICE, E_USER_NOTICE, E_DEPRECATED, E_USER_DEPRECATED),
 
 	/*
 	|--------------------------------------------------------------------------
