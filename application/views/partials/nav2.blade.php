7e8d9d9275998e4b5d0fb33b81880f3efd06f0d3
Mark Myers <marcusmyers@gmail.com>
2014-04-01 14:23:31 -0400

    Updated navigation to work correctly

--- application/views/partials/nav2.blade.php
+++ application/views/partials/nav2.blade.php
@@ -3,6 +3,7 @@
 	<ul class="nav">
 		<li class='active'><a href="/building">Home</a></li>
 		<li><a href="/building/budget">Budget</a></li>
+		<li><a href="/building/budget/edit">Edit Budget</a></li>
 		<li><a href="/building/revenue">Revenue</a></li>
 		<li><a href="/building/export">Export for Records</a></li>
 		<li><a href="/admin/logout">Logout</a></li>
