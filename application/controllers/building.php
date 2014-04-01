6e01fbf396f9f78c85bb0f6facbec57783dc7800
Mark Myers <marcusmyers@gmail.com>
2014-04-01 15:39:27 -0400

    DUMP arrFundTotals

--- application/controllers/building.php
+++ application/controllers/building.php
@@ -87,6 +87,7 @@ class Building_Controller extends Base_Controller {
 										}
 									}
 									$arrFundTotals[$prevFund] = $fundTotal;
+									var_dump($arrFundTotals);
 								}
 							return View::make('admin.buildingbudget', array('budgets'=>$arrBudgets,'expended'=>$arrExpended, 'fundTotal'=>$arrFundTotals,'edit'=>true, 'proposed'=>$arrProposed, 'budgettotal'=>$budgettotal->amount))->nest('nav','partials.nav2');
 						}
