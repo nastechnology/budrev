3298ae59485d98b98a3fbad840260bcaaa905f83
Mark Myers <marcusmyers@gmail.com>
2014-04-28 09:45:50 -0400

    Added debug code

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -991,7 +991,7 @@ class Admin_Controller  extends Base_Controller {
 
 						foreach ($values as $name=>$value) {
 							list($p,$buildingbudget_id) = explode("-",$name);
-							// echo "BuildingBudgetID :: " . $buildingbudget_id . "<br/>Before Setting Amount";
+							echo "BuildingBudgetID :: " . $buildingbudget_id . "<br/>Before Setting Amount";
 							$bbuildingproposed = BuildingBudgetProposed::where('buildingbudget_id','=',$buildingbudget_id)->first();
 							// var_dump($bbuildingproposed);
 
